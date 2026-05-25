6990011034ffc5046b54410756cc80a237596440
liaanboro <kabinoboro@gmail.com>
2026-05-25 11:36:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-  base: './', // Important for UXP plugins so paths are relative
+  plugins: [react()],
+  base: '/subtitle-generator/'
 })
